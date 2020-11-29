5945e0f96307881eeeb074d185fb6b0af379a564
JonLyn <jonlyn22@gmail.com>
2020-11-29 18:59:07 -0500

    items are rendering to screen correcty and under the correct dept :rooster:

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Component } from 'react';
 import firebase from './firebase.js';
 import InputNewItem from './InputNewItem.js';
-import ShowList from './ShowList.js';
+// import ShowList from './ShowList.js';
 
 class App extends Component {
 
@@ -20,42 +20,71 @@ class App extends Component {
 
     dbRef.on('value', (data) => {
       const firebaseDataObject = data.val();
-      // console.log(firebaseDataObject)
 
-      let deptArray = [];
-      let itemArray = [];
+      console.log(firebaseDataObject);
+      let deptArray = Object.keys(firebaseDataObject).map((key) => [(key), firebaseDataObject[key]]);
 
-      for (let deptKey in firebaseDataObject) {
-        // console.log(deptKey);
-        // console.log(firebaseDataObject);
+      // console.log(deptArray[1])
+
+      // let itemArray = Object.values(deptArray[1])
+      // console.log(itemArray);
+      // let itemArray = Object.keys(deptArray).map((keys) => [(keys), deptArray[keys]]);
+      // console.log('items', itemArray);
+
+
+      this.setState({
+        departments: deptArray
+      })
+      console.log(this.state.departments)
+
+      // let itemArray = []
+
+      // let itemArray = [];
+      // let deptArray = [];
+      // for (let deptKey in firebaseDataObject) {
+
+      //   const individualChicken = firebaseDataObject[deptKey];
+      //   console.log(individualChicken);
         
-        // const eachItem = [firebaseDataObject[deptKey]];
-        // const formattedDepts = {
-        //   dept: deptKey,
-        //   items: eachItem
-        // };
-        // deptArray.push(formattedDepts);
+      //   const formattedDepts = {
+      //     dept: deptKey,
+      //     items: [individualChicken]
+      //   }
+      //   deptArray.push(formattedDepts);
 
-      
-        // this.setState({
-        //   departments: deptArray
-        // })
+      //   console.log('format', formattedDepts);
 
-        // console.log('depos', this.state.departments)
-       
+      //   this.setState({
+      //     departments: testArray
+      //   })
 
-        for (let itemKey in firebaseDataObject[deptKey]) {
 
-          const individualItem = firebaseDataObject[deptKey][itemKey];
+
+        // console.log('state', this.state.departments)
+  
+
+        
+    //     for (let itemKey in firebaseDataObject[deptKey]) {
+    //       console.log('itemkey', itemKey);
+
+    //       const individualItem = firebaseDataObject[deptKey][itemKey];
+
+    //       console.log('for in item', individualItem);
+    //       itemArray.push(individualItem);
+        
+    //       const eachItem = itemArray
+    //       console.log(eachItem);
           
-          itemArray.push(individualItem);
-          this.setState({
-            items: individualItem
-          })
-          console.log(itemArray);
-        }
-      }
-    })
+          
+    //       console.log('depos', this.state.departments)
+    //       console.log('items', this.state.items)
+    //     }
+    //   }
+    //   console.log('itemmssss', itemArray);
+    //   this.setState({
+    //     items: itemArray
+    // })
+  })
   }
 
   render() { 
@@ -64,19 +93,36 @@ class App extends Component {
             <InputNewItem />
             {
               this.state.departments.map((singleDept, i) => {
-                console.log(singleDept);
                 return (
-                  <ul  key={i}>
+                  <ul key={i}>
                     <li>
-                      <p>{singleDept.dept}</p>
-                      <p>{singleDept.items}</p>
+                      <p>{singleDept[0]}</p>
+                      {
+                        Object.values(singleDept[1]).map((item, i) => {
+                          return (
+                            <p key={i}>{item}</p>
+                          )
+                        })
+                      }
+                       
 
+                      {/* <p>{singleDept[1]}</p> */}
+                        {/* {
+                          this.state.items.map((singleItem, index) => {
+                            return (
+                              <ul key={index}>
+                                <li>
+                                  <p>{singleItem}</p>
+                                </li>
+                              </ul>
+                            )
+                          })
+                        } */}
                     </li>
                     {/* <ShowList 
                       dept={singleDept.dept}
                       items={singleDept.items}
                     /> */}
-
                   </ul>
                   
                 )
