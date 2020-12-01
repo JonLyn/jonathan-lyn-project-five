3500d35ae9861218a7973a1648f00c950caa1f15
JonLyn <jonlyn22@gmail.com>
2020-12-01 17:34:11 -0500

    adding true or false toggle for completed items :zany_face:

--- src/App.js
+++ src/App.js
@@ -34,23 +34,28 @@ class App extends Component {
         let deptArray = Object.keys(firebaseDataObject).map((key) => {
           return [(key), firebaseDataObject[key]];
         })
-        
+        console.log('componentDidMount', deptArray)
         this.setState({
           departments: deptArray,
           enterItem: ''
         })
       }
       
-      const objects = this.state.departments[1][1]
-      console.log('OG', this.state.departments)
-      console.log('objects', objects)
+      // const objects = this.state.departments[1][1]
+      // console.log('OG', this.state.departments)
+      // console.log('objects', objects)
 
-      const objectsArray = Object.values(objects).forEach((itemArray) => {
-        if (itemArray[0] === true) {
-          console.log('true')
-        }
-      })
-      console.log(objectsArray)
+      // this.state.departments.forEach((deptArray) => {
+      //   Object.values(objects).forEach((itemArray) => {
+      //   if (itemArray[0] === true) {
+          // return ['yes']
+          // console.log(itemArray[0])
+          // console.log('true')
+        // }
+      // })
+      // })
+     
+      // console.log(objectsArray)
 
       // for (let key in objects) {
       //   console.log('KEY', key)
@@ -89,9 +94,24 @@ class App extends Component {
   markCompleted = (itemKey, dept) => {
     const dbRef = firebase.database().ref();
     let completePath = `/${dept[0]}/${itemKey}/`;
+    
+    // this.state.departments.map((singleDept, i) => {
+    //         let dept = singleDept;
+    //         let itemsArray = [];
+    //         let completed = []
+    //         for (let idKey in singleDept[1]) {
+    //           const itemIds = [idKey, singleDept[1][idKey]]
+    //           itemsArray.push(itemIds)
+    //           completed.push(singleDept[1][idKey][0])
+    //           console.log('single1', completed)
+    //         } 
+    //       })
+
     dbRef.child(completePath).update({0: true});
-    console.log('dept', dept);
-    console.log('itemkey', itemKey)
+
+
+    // console.log('dept', dept);
+    // console.log('itemkey', itemKey)
   }
 
   updateItem = (e, dept) => {
@@ -106,79 +126,57 @@ class App extends Component {
 
   }
 
-
-
   render() { 
     return (
       <div className='wrapper'>
         <InputNewItem />
         <h2>{this.state.enterItem}</h2>
 
-        {/* <button onClick={this.handleReload}>Load prior list</button> */}
         {
           this.state.departments.map((singleDept, i) => {
             let dept = singleDept;
             let itemsArray = [];
+            let completed = []
             for (let idKey in singleDept[1]) {
               const itemIds = [idKey, singleDept[1][idKey]]
               itemsArray.push(itemIds)
-            }
+              completed.push(singleDept[1][idKey][0])
+              console.log('single1', completed)
+            } 
 
-            console.log(itemsArray)
-
-
+            console.log(completed)
             return (
-              
-              <ul key={i}>
-                <li>
-                  <p>{singleDept[0]}</p>
+              <div key={i}>
+                <ul>
+                  <li>
+                    <p>{singleDept[0]}</p>
                   
-                  {
-                    itemsArray.map((item) => {
-                      return (
-                    
+                    {
+                      itemsArray.map((item, index) => {
+    
+                        console.log('ITEM', item[1])
+                        console.log('INDEX', completed[index])
+                        return (
                           <div key={item[0]}>
-                            {/* {item  ?
-                              
-                              
+                            {
+                              (completed[index])
+                              ? <p className="item">{item[1]}</p>
+                              : <p>test</p>
+
                             }
-                             */}
-                            <p className="item">{item[1]}</p>
+
+                            {/* <p className="item">{item[1]}</p> */}
                             
                             <button onClick={ () => { this.removeItemFromDb(item[0], dept) }}>Remove</button>
 
                             <button onClick={ () => { this.markCompleted(item[0], dept) } }>Complete</button>
-
-                            <label htmlFor="location"> Department or aisle: </label>
-                            <select name="location" id="location" onChange={ (e) => {this.updateItem(e, dept)} }>
-                                <option value="Unknown">Unknown</option>
-                                <option value="Deli">Deli</option>
-                                <option value="Meat">Meat</option>
-                                <option value="Produce">Produce</option>
-                                <option value="Seafood">Seafood</option>
-                            </select>
-
                           </div>
-                            // {/* <RemoveItem 
-                            //   item={item[1]}
-                            //   remove={ () => { this.removeItem(i) }}
-                            // /> */}
-                      
-                      
-                      )
-                    })
-                  }
-                </li>
-
-
-
-                {/* <ShowList 
-                  dept={singleDept.dept}
-                  items={singleDept.items}
-                /> */}
-
-
-              </ul>
+                        )
+                      })
+                    }
+                  </li>
+                </ul>
+              </div>
             )
           })
         }
@@ -188,3 +186,52 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
+{/* return (
+            this.state.departments.forEach((dept) => {
+              // console.log(dept[0]);
+              // console.log('dept1', dept[1]);
+              for (let product in dept[1]) {
+                console.log(dept[1][product]);
+                
+                dept[1][product].map((item) => {
+                  // console.log('item equals', item)
+                  <ul key={'test'}>
+                    <li>
+                      <p>hello</p>
+                    </li>
+                  </ul>
+                })
+
+
+                }
+              })
+          ) */}
+
+
+
+           // {/* <RemoveItem 
+                            //   item={item[1]}
+                            //   remove={ () => { this.removeItem(i) }}
+                            // /> */}
+
+
+
+
+       {/* <ShowList 
+                  dept={singleDept.dept}
+                  items={singleDept.items}
+                /> */}
+
+
+                // <label htmlFor="location"> Department or aisle: </label>
+                //               <select name="location" id="location" onChange={ (e) => {this.updateItem(e, dept)} }>
+                //                   <option value="Unknown">Unknown</option>
+                //                   <option value="Deli">Deli</option>
+                //                   <option value="Meat">Meat</option>
+                //                   <option value="Produce">Produce</option>
+                //                   <option value="Seafood">Seafood</option>
+                //               </select>
