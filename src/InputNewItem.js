5d438011d787bd63249123db766c36aa288276b7
JonLyn <jonlyn22@gmail.com>
2021-01-04 12:14:42 -0500

    added background colour to depts to create more distinction

--- src/InputNewItem.js
+++ src/InputNewItem.js
@@ -5,88 +5,89 @@ import DepartmentList from './DepartmentList.js';
 
 class InputNewItem extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            userInput: '',
-            dept: 'choose location',
-        }
+  constructor() {
+    super();
+    this.state = {
+      userInput: '',
+      dept: 'choose location',
     }
+  }
 
-    // set user input to state
-    handleInputChange = (e) => {
-        this.setState({
-            userInput: e.target.value
-        }) 
-    }
+  // set user input to state
+  handleInputChange = (e) => {
+    this.setState({
+      userInput: e.target.value
+    })
+  }
 
-    // set dept chosen to state
-    handleDeptChange = (e) => {
-        this.setState({
-            dept: e.target.value
-        })
-    }
+  // set dept chosen to state
+  handleDeptChange = (e) => {
+    this.setState({
+      dept: e.target.value
+    })
+  }
 
-    // submit function to send user inputs to firebase
-    handleSubmit = (e) => {
-        e.preventDefault();
-        let inputted = this.state.userInput
-        let itemNotCompletedArray = [false, inputted]
-        const dbRef = firebase.database().ref();
-        dbRef.child(this.state.dept).push(itemNotCompletedArray);
-        this.setState({
-            userInput: ''
-        })
-    }
+  // submit function to send user inputs to firebase
+  handleSubmit = (e) => {
+    e.preventDefault();
 
-    render() { 
-        return (
-            <form 
-                // condition to check if both input and location are chosen
-                onSubmit={(this.state.userInput !== '' && this.state.dept !== 'choose location') 
-                ? this.handleSubmit 
-                : null} 
-                className='newItemForm'
-            >
-                <div className='itemInput'>
-                    {/* item input text area */}
-                    <label htmlFor='newItem'>Item: </label>
-                    <input 
-                        className='inputField'
-                        type='text'
-                        id='newItem'
-                        value={this.state.userInput}
-                        onChange={this.handleInputChange}
-                        placeholder='Enter an item'
-                        required
-                    />
-                </div>
-                <div className='deptInput'>
-                    {/* location dropdown */}
-                    <label htmlFor='location'> Dept / aisle: </label>
-                    <select 
-                        className='selectField'
-                        name='location' id='location' 
-                        onChange={this.handleDeptChange} 
-                        value={this.state.dept}>
-                            {/* location selections component */}
-                            <DepartmentList 
-                        required    
-                    />  
-                    </select>
-                    {/* button to submit user inputs */}
-                    <button className='addButton' aria-label='add item to list'>
-                        <FontAwesomeIcon 
-                            className='icon'     
-                            icon={['far', 'plus-square']} 
-                            type='submit' 
-                        /> 
-                        <span className='addText'>Add</span>     
-                    </button>
-                </div>
-            </form>
-        )
-    }
+    let inputted = this.state.userInput
+    let itemNotCompletedArray = { 0: false, inputted }
+    const dbRef = firebase.database().ref();
+    dbRef.child(this.state.dept).push(itemNotCompletedArray);
+    this.setState({
+      userInput: ''
+    })
+  }
+
+  render() {
+    return (
+      <form
+        // condition to check if both input and location are chosen
+        onSubmit={(this.state.userInput !== '' && this.state.dept !== 'choose location')
+          ? this.handleSubmit
+          : null}
+        className='newItemForm'
+      >
+        <div className='itemInput'>
+          {/* item input text area */}
+          <label htmlFor='newItem'>Item: </label>
+          <input
+            className='inputField'
+            type='text'
+            id='newItem'
+            value={this.state.userInput}
+            onChange={this.handleInputChange}
+            placeholder='Enter an item'
+            required
+          />
+        </div>
+        <div className='deptInput'>
+          {/* location dropdown */}
+          <label htmlFor='location'> Dept / aisle: </label>
+          <select
+            className='selectField'
+            name='location' id='location'
+            onChange={this.handleDeptChange}
+            value={this.state.dept}>
+            {/* location selections component */}
+            <DepartmentList
+              required
+            />
+          </select>
+          {/* button to submit user inputs */}
+          <button className='addButton' aria-label='add item to list'>
+            <FontAwesomeIcon
+              className='icon'
+              icon={['far', 'plus-square']}
+              type='submit'
+            />
+            <span className='addText'>Add</span>
+          </button>
+        </div>
+      </form>
+    )
+  }
 }
 
 export default InputNewItem;
